0d22a5fb7b0201ad41fd41a6cfa4383c5ccdac2a
Antoine Pizon <pizon.antoine@gmail.com>
2017-11-21 23:06:17 +0100

    Prepared car class for future cornering implementation

--- Car.cpp
+++ Car.cpp
@@ -1,46 +1,68 @@
 #include "stdafx.h"
 #include "Car.hpp"
 
-Car::Car(ShaderProgramPtr shader) : HierarchicalRenderable(shader), m_mass(1500.0f), m_torque(3000.0f), m_torqueb(1000.0f), m_brakes(10000.0f), m_wradius(0.3f) {
+Car::Car(ShaderProgramPtr shader, float mass, float engine, float brakes) : HierarchicalRenderable(shader), m_mass(mass), m_engine(engine), m_brakes(brakes) {
 	//Car
 	setParentTransform(glm::translate(glm::mat4(1.0f), glm::vec3(0.0f, 0.0f, 1.0f)));
 	setLocalTransform(glm::mat4(1.0f));
 
-	//Body
-	m_body = std::make_shared<KeyframedMeshRenderable>(shader, "meshes/car.obj");
-	m_body->setParentTransform(glm::mat4(1.0));
-	m_body->setLocalTransform(glm::mat4(1.0));
+	//Frame
+	m_frame = std::make_shared<KeyframedMeshRenderable>(shader, "meshes/car.obj");
+	m_frame->setParentTransform(glm::mat4(1.0));
+	m_frame->setLocalTransform(glm::mat4(1.0));
 
 	//Wheels
 	for (int i = 0; i < 4; i++) {
 		m_wheels[i] = std::make_shared<KeyframedMeshRenderable>(shader, "meshes/wheel.obj");
 		m_wheels[i]->setLocalTransform(glm::mat4(1.0));
-		HierarchicalRenderable::addChild(m_body, m_wheels[i]);
+		HierarchicalRenderable::addChild(m_frame, m_wheels[i]);
 	}
 
 	m_wheels[0]->setParentTransform(glm::translate(glm::mat4(1.0f), glm::vec3(0.98, 0.8, 0)));
 	m_wheels[1]->setParentTransform(glm::translate(glm::mat4(1.0f), glm::vec3(0.98, -0.8, 0)));
 	m_wheels[2]->setParentTransform(glm::translate(glm::mat4(1.0f), glm::vec3(-1.15, 0.8, 0)));
 	m_wheels[3]->setParentTransform(glm::translate(glm::mat4(1.0f), glm::vec3(-1.15, -0.8, 0)));
-
-	m_lastTime = 0;
-
+	
 	m_state.direction = NONE;
 	m_state.turn = NONE;
 
-	m_velocity = glm::vec3(0.0f, 0.0f, 0.0f);
-	m_acceleration = glm::vec3(0.0f, 0.0f, 0.0f);
+	m_lastTime = 0;
 
-	m_ftotal = glm::vec3(0.0f, 0.0f, 0.0f);
-	m_ftraction = glm::vec3(0.0f, 0.0f, 0.0f);
-	m_fbrake = glm::vec3(0.0f, 0.0f, 0.0f);
-	m_fdrag = glm::vec3(0.0f, 0.0f, 0.0f);
-	m_frr = glm::vec3(0.0f, 0.0f, 0.0f);
-	m_fg = glm::vec3(0.0f, 0.0f, 0.0f);
+	//Angles
+	m_alphaF = 0.0f;
+	m_alphaR = 0.0f;
+	m_sigma = 0.0f;
+
+	//Torque
+	m_gear = 10.0f;
+	m_torque = 0.0f;
+
+	//Wheels
+	m_wradius = 0.5f;
+	m_wrotation = 0.0f;
+	m_torquecar = 0.0f;
+
+	//Movement
+	//m_position = glm::vec3(0, 0, 1);
+	m_velocity = glm::vec3(0, 0, 0);
+	m_acceleration = glm::vec3(0, 0, 0);
+	//m_angposition = glm::vec3(0, 0, 0);
+	m_angvelocity = glm::vec3(0, 0, 0);
+	m_angacceleration = glm::vec3(0, 0, 0);
+
+	//Forces
+	m_FlatF = glm::vec3(0, 0, 0);
+	m_FlatR = glm::vec3(0, 0, 0);
+	m_Frr = glm::vec3(0, 0, 0);
+	m_Fdrag = glm::vec3(0, 0, 0);
+	m_Ftraction = glm::vec3(0, 0, 0);
+	m_Fresistance = glm::vec3(0, 0, 0);
+	m_Fg = glm::vec3(0, 0, 0);
+	m_Ftotal = glm::vec3(0, 0, 0);
 }
 
 void Car::init() {
-	HierarchicalRenderable::addChild(shared_from_this(), m_body);
+	HierarchicalRenderable::addChild(shared_from_this(), m_frame);
 }
 
 void Car::do_keyPressedEvent(sf::Event& e) {
@@ -91,12 +113,18 @@ void Car::do_animate(float time) {
 	float dt = time - m_lastTime;
 	m_lastTime = time;
 
-	m_acceleration = m_ftotal / m_mass;
+	m_acceleration = m_Ftotal / m_mass;
 	m_velocity += m_acceleration * dt;
+	//m_position += m_velocity * dt;
+
+	m_angacceleration.z = m_torquecar / m_mass;
+	m_angvelocity.z += m_angacceleration.z * dt;
+	//m_angposition.z += m_angvelocity.z * dt;
 
 	std::cout << "speed :" << ceil(m_velocity.x * 3.6f) << "kmh /  fps:" << ceil(1.0f / dt) << std::endl;
 
-	setParentTransform(glm::translate(getParentTransform(), dt * m_velocity));
+	setParentTransform(glm::translate(getParentTransform(), m_velocity * dt));
+	setParentTransform(glm::rotate(getParentTransform(), m_angvelocity.z  * dt, glm::vec3(0, 0, 1)));
 	m_viewer->getCamera().setViewMatrix(glm::lookAt(glm::vec3(getParentTransform()[3]) + glm::vec3(-15.0f, 0.0f, 7.5f), glm::vec3(getParentTransform()[3]), glm::vec3(0, 0, 1)));
 }
 
@@ -104,39 +132,38 @@ void Car::computeTotalForce() {
 	computeDragForce();
 	computeResistanceForce();
 	//computeGravity();
-	m_ftotal = m_fdrag + m_frr + m_fg;
+	m_Ftotal = m_Fdrag + m_Frr + m_Fg;
 
-	if (m_state.direction == FORWARD && m_velocity.x >= 0 || m_state.direction == BACKWARD && m_velocity.x <= 0) {
+	if (m_state.direction == FORWARD && m_velocity.x >= 0) {
+		m_gear = 10.0f;
 		computeTractionForce();
-		m_ftotal += m_ftraction;
+		m_Ftotal.x += m_Ftraction.x;
 	}
-	else {
-		computeBrakeForce();
-		m_ftotal += m_fbrake;
+	else if (m_state.direction == BACKWARD && m_velocity.x <= 0) {
+		m_gear = 2.0f;
+		computeTractionForce();
+		m_Ftotal.x += m_Ftraction.x;
+	}
+	else if (m_state.direction != NONE) {
+		m_Ftotal.x += m_state.direction * m_brakes;
 	}
 }
 
 void Car::computeTractionForce() {
-	if(m_state.direction == FORWARD)
-		m_ftraction.x = m_torque / m_wradius;
-	if (m_state.direction == BACKWARD)
-		m_ftraction.x = -m_torqueb / m_wradius;
-}
-
-void Car::computeBrakeForce() {
-	m_fbrake.x = m_state.direction * m_brakes;
+	m_torque = m_state.direction * m_gear * m_engine;
+	m_Ftraction.x = m_torque / m_wradius;
 }
 
 void Car::computeDragForce() {
-	m_fdrag = -AERODRAG * m_velocity * glm::length(m_velocity);
+	m_Fdrag = -CDRAG * m_velocity * glm::length(m_velocity);
 }
 
 void Car::computeResistanceForce() {
-	m_frr = -CRR * m_velocity;
+	m_Frr = -CRR * m_velocity;
 }
 
 void Car::computeGravity() {
-	m_fg.z = -m_mass * GRAVITY;
+	m_Fg.z = -m_mass * GRAVITY;
 }
 
 ////Old
--- Car.hpp
+++ Car.hpp
@@ -11,7 +11,7 @@
 //#define ACCR		360.0f
 
 #define GRAVITY		9.81f
-#define AERODRAG	10.0f
+#define	CDRAG		10.0f
 #define CRR			0.01f
 
 #define NONE		0
@@ -22,7 +22,7 @@
 
 class Car : public HierarchicalRenderable, public std::enable_shared_from_this<Car> {
 public:
-	Car(ShaderProgramPtr shader);
+	Car(ShaderProgramPtr shader, float mass, float engine, float brakes);
 	void init();
 	void do_keyPressedEvent(sf::Event& e);
 	void do_keyReleasedEvent(sf::Event& e);
@@ -31,7 +31,6 @@ public:
 	
 	void computeTotalForce();
 	void computeTractionForce();
-	void computeBrakeForce();
 	void computeDragForce();
 	void computeResistanceForce();
 	void computeGravity();
@@ -42,23 +41,44 @@ private:
 		int turn;
 	};
 
+	//Consts
 	const float m_mass;
-	const float m_torque;
-	const float m_torqueb;
+	const float m_engine;
 	const float m_brakes;
-	const float m_wradius;
 
+	//Angles
+	float m_alphaF;
+	float m_alphaR;
+	float m_sigma;
+	
+	//Traction
+	float m_gear;
+	float m_torque;
+
+	//Wheels
+	float m_wradius;
+	float m_wrotation;
+	float m_torquecar;
+
+	//Movement
+	glm::vec3 m_position;
 	glm::vec3 m_velocity;
 	glm::vec3 m_acceleration;
+	glm::vec3 m_angacceleration;
+	glm::vec3 m_angvelocity;
+	glm::vec3 m_angposition;
 
-	glm::vec3 m_ftotal;
-	glm::vec3 m_ftraction;
-	glm::vec3 m_fbrake;
-	glm::vec3 m_fdrag;
-	glm::vec3 m_frr;
-	glm::vec3 m_fg;
+	//Forces
+	glm::vec3 m_FlatF;
+	glm::vec3 m_FlatR;
+	glm::vec3 m_Frr;
+	glm::vec3 m_Fdrag;
+	glm::vec3 m_Ftraction;
+	glm::vec3 m_Fresistance;
+	glm::vec3 m_Fg;
+	glm::vec3 m_Ftotal;
 
-	KeyframedMeshRenderablePtr m_body;
+	KeyframedMeshRenderablePtr m_frame;
 	KeyframedMeshRenderablePtr m_wheels[4];
 	float m_lastTime;
 	State m_state;
@@ -66,7 +86,9 @@ private:
 
 typedef std::shared_ptr<Car> CarPtr;
 
-//TODO
+//---------------------------NOTES---------------------------
+//
+//---------------------------LINEAR--------------------------
 //Position					P = P + v * dt
 //Velocity					V = V + a * dt
 //Acceleration				A = F / M				
@@ -79,3 +101,89 @@ typedef std::shared_ptr<Car> CarPtr;
 //Aerodynamic drag			Fdrag = -K * V^2
 //Rolling resistance		Frr = -K * V
 //Gravity					Fg = -m * G
+//
+//--------------------------CORNERING-----------------------
+//
+//Cornering					Fcornering = Ca * alpha
+//Slip angle front			alpha = atan((Vlat + w*b)/Vlong) - sigma * sign(Vlong)		b = distance between CG and front axle
+//																						sigma = steering angle
+//																						w = angular vel
+//Slip angle rear			alpha = atan((Vlat - w*c)/Vlong)							c = distance between CG and rear axle
+//Resulting torque			T = cos(sigma) * Flat,front * b - Flat,rear * C
+//
+//Angular acc				wacc = T / inertia
+//Angular vel				w = w + wacc * dt
+//Angular pos				w. = w. + w * dt
+
+
+//------------------------ALGORITHM------------------------
+//
+//1. V.x = Vlong, V.y = Vlat
+//2. Compute alpha : alphaF = atan((V.y + w*F)/V.x) - sigma * sign(V.x)		for front
+//					 alphaR = atan((V.y - w*R)/V.x)							for rear
+//3. FlatR = cornering * alphaR
+	//FlatF = cornering * alphaF
+//4. Cap Flat to max
+//5. Flat *= load
+//6.7.8.9.10 Compute engine torque Te
+	//6. TOR = Vx 60 * gk*G / (2PI * rw)
+	//7. Clamp the engine turn over rate from 6 to the defined redline
+	//8. If use automatic transmission call automaticTransmission() function
+	//to shift the gear
+	//9. Compute the constant that define the torque curve line from the
+	//engine turn over rate
+	//10. From 9, compute the maximum engine torque, Te
+//11. Tw = Te * gear
+//12. Ftraction = Tw / Wradius
+	//13. If the player is braking Ftraction = -brakes
+//15. Frr = -Crr * V
+//16. Fdrag = -Cdrag * V * |V|
+//17. Fresistance = Frr + Fdrag
+//18. Ftotal.x = Ftraction + Flatf * sin(sigma) * Fresistance.x
+	//Ftotal.y = Flatr + Flatf * cos(sigma) * Fresistance.y
+	//Ftotal.z = Fg
+//19. Tcar = cos(sigma) * Flatf * b – Flatr * c
+//20. Acc = Ftotal / m																					DONE
+//21. angAcc = Tcar / I																					
+//22. V += dt * Acc																						DONE
+//23. P += dt * V																						DONE
+//24. Move the camera																					DONE
+//25. angV += dt * angAcc																				DONE
+//26. angP += dt * angV																					DONE
+//27. Wrot = V / Wradius
+//28. Render the car																					DONE
+
+//-----------------------GLOSSARY-----------------------
+//
+//P				=	m_position
+//V				=	m_velocity
+//Acc			=	m_acceleration
+//angAcc		=	m_angacceleration
+//angV			=	m_angvelocity
+//angP			=	m_angposition
+//
+//alphaF		=	m_alphaF
+//alphaR		=	m_alphaR
+//sigma			=	m_sigma
+//m				=	m_mass
+//load			=	m_mass/2
+//Te			=	m_engine
+//gear			=	m_gear
+//Tw			=	m_torque
+//brakes		=	m_brakes
+//Tcar			=	m_torquecar
+//Wradius		=	m_wradius
+//Wrot			=	m_wrotation
+//
+//Crr			=	CRR
+//Cdrag			=	CDRAG
+//G				=	GRAVITY
+//
+//FlatF			=	m_FlatF
+//FlatR			=	m_FlatR
+//Frr			=	m_Frr
+//Fdrag			=	m_Fdrag
+//Ftraction		=	m_Ftraction
+//Fresistance	=	m_Fresistance
+//Fg			=	m_Fg
+//Ftotal		=	m_Ftotal
