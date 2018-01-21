161b02fb24df8e88f490d398a2cf4fb33bc0f162
Antoine Pizon <pizon.antoine@gmail.com>
2018-01-21 12:48:24 +0000

    Rapport final

--- OpenGL/shaders/textureFragment.glsl
+++ OpenGL/shaders/textureFragment.glsl
@@ -62,7 +62,10 @@ uniform PointLight pointLight[MAX_NR_POINT_LIGHTS];
 uniform int numberOfSpotLight;
 uniform SpotLight spotLight[MAX_NR_SPOT_LIGHTS];
 
+uniform sampler2D texSampler;
+
 // Surfel: a SURFace ELement. All coordinates are in world space
+in vec2 surfel_texCoord;
 in vec3 surfel_position;
 in vec4 surfel_color;
 in vec3 surfel_normal;
@@ -101,7 +104,7 @@ vec3 computePointLight(PointLight light, vec3 surfel_to_camera)
     float distance = length( surfel_to_light );
     surfel_to_light *= float(1) / distance;
     float diffuse_factor = max(dot(surfel_normal, surfel_to_light), 0.0);
-    
+
     // Specular shading
     vec3 reflect_direction = reflect(-surfel_to_light, surfel_normal);
     float specular_factor = pow(max(dot(surfel_to_camera, reflect_direction), 0.0), material.shininess);
@@ -110,7 +113,7 @@ vec3 computePointLight(PointLight light, vec3 surfel_to_camera)
     float attenuation = 1.0f / (light.constant + light.linear * distance + light.quadratic * (distance * distance));
     //float attenuation = 1.0;
 
-    // Combine results    
+    // Combine results
     vec3 ambient  = attenuation *                   light.ambient  * material.ambient ;
     vec3 diffuse  = attenuation * diffuse_factor  * light.diffuse  * material.diffuse ;
     vec3 specular = attenuation * specular_factor * light.specular * material.specular;
@@ -126,7 +129,7 @@ vec3 computeSpotLight(SpotLight light, vec3 surfel_to_camera)
     float distance = length( surfel_to_light );
     surfel_to_light *= float(1) / distance;
     float diffuse_factor = max(dot(surfel_normal, surfel_to_light), 0.0);
-    
+
     // Specular
     vec3 reflect_direction = reflect(-surfel_to_light, surfel_normal);
     float specular_factor = pow(max(dot(surfel_to_camera, reflect_direction), 0.0), material.shininess);
@@ -135,16 +138,16 @@ vec3 computeSpotLight(SpotLight light, vec3 surfel_to_camera)
     //float intensity = 1.0;
     float cos_theta = dot(surfel_to_light, -light.spotDirection);
     float intensity = clamp( (cos_theta - light.outerCutOff ) / ( light.innerCutOff - light.outerCutOff ), 0, 1 );
-		    
+
     // Attenuation
     //float attenuation = 1.0;
     float attenuation = 1.0f / (light.constant + light.linear * distance + light.quadratic * (distance * distance));
 
-    // Combine results    
+    // Combine results
     vec3 ambient  =             attenuation *                   light.ambient  * material.ambient ;
     vec3 diffuse  = intensity * attenuation * diffuse_factor  * light.diffuse  * material.diffuse ;
     vec3 specular = intensity * attenuation * specular_factor * light.specular * material.specular;
-    
+
     return (ambient + diffuse + specular);
 }
 
@@ -166,5 +169,6 @@ void main()
     for(int i=0; i<clampedNumberOfSpotLight; ++i)
         tmpColor += computeSpotLight(spotLight[i], surfel_to_camera);
 
-    outColor = vec4(tmpColor,1.0);
+    vec4 textureColor = texture(texSampler, surfel_texCoord);
+    outColor = textureColor*vec4(tmpColor,1.0);
 }
